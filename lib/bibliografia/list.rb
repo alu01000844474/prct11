b909c7d1626040bf6966113bec7010a95e79bbd1
Eleazar Díaz Delgado <eleazardzdo@gmail.com>
2015-11-10 14:38:25 +0000

    Implemento take first y la clase

--- lib/bibliografia/list.rb
+++ lib/bibliografia/list.rb
@@ -1,4 +1,20 @@
 module Bibliografia
 	# Struct of a list
 	Node = Struct.new(:value, :next)
+
+	class List
+		def initialize(val)
+			@node = Node.new(val, nil)
+		end
+
+		def takeFirst
+			if @node != nil
+				value = @node.value
+				@node = nil
+				return value
+			else
+				return nil
+			end
+		end
+	end
 end
