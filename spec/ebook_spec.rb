d72cd5d94c4b3a4b90e674053ed3d74b27d6585e
alu0100844474 <alu0100844474@ull.edu.es>
2015-11-17 19:53:10 +0000

    Debe estar hererado de Bibliografia Ebook

--- spec/ebook_spec.rb
+++ spec/ebook_spec.rb
@@ -4,6 +4,11 @@ require 'date'
 describe Bibliografia do
 	describe Bibliografia::Ebook do
 		before :all do
+		    @ebook = Bibliografia::Ebook.new(["Dave Thomas"],
+			"Programming Ruby ",
+			"The Facets of Ruby",
+			"Pragmatic Bookshelf",4,Date.new(2013,7,7),
+			"google.es")
 		end
 		
 		it "Debe poderse pasarle todos los argumentos necesarios" do
@@ -12,6 +17,18 @@ describe Bibliografia do
 			"The Facets of Ruby",
 			"Pragmatic Bookshelf",4,Date.new(2013,7,7),
 			"google.es")).to_not be nil
+		end
+		
+		it "Debe ser un Ebook" do
+		    expect(@ebook.is_a?Bibliografia::Ebook).to eq true 
+		end
+		
+		it "Debe se una subclase de Bibliografia" do
+		    expect(@ebook.is_a?Object).to eq true 
+		end
+	    
+	    it "Debe se una subclase de Bibliografia" do
+		    expect(@ebook.is_a?BasicObject).to eq true 
 		end
 	end
 end
