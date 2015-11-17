8aab6bc0c7b918def1019b5901f498ce821ef0ac
alu0100844474 <alu0100844474@ull.edu.es>
2015-11-17 20:01:12 +0000

    Se espera una clase revista que se le puedan pasar una serie de argumentos

--- spec/revista_spec.rb
+++ spec/revista_spec.rb
@@ -4,6 +4,19 @@ require 'date'
 describe Bibliografia do
 	describe Bibliografia::Revista do
 		before :all do
+		    @revista = Bibliografia::Revista.new(["Dave Thomas"],
+			"Programming Ruby ",
+			"The Facets of Ruby",
+			"Pragmatic Bookshelf",4,Date.new(2013,7,7),
+			"777744545-4-7874")
+		end
+		
+		it "Debe poderse pasarle todos los argumentos necesarios" do
+		    expect(Bibliografia::Revista.new(["Dave Thomas"],
+			"Programming Ruby ",
+			"The Facets of Ruby",
+			"Pragmatic Bookshelf",4,Date.new(2013,7,7),
+			"777744545-4-7874")).to_not be nil
 		end
 	end
 end
