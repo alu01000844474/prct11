ae18a1b136c91390e6e6abe2790340aae6e7ba19
alu0100844474 <alu0100844474@ull.edu.es>
2015-11-17 19:44:27 +0000

    Se le pueden pasar todos los argumentos

--- lib/bibliografia/ebook.rb
+++ lib/bibliografia/ebook.rb
@@ -2,5 +2,9 @@ require "bibliografia"
 
 module Bibliografia
     class Ebook < Bibliografia
+        def initialize(autores, titulo, serie, edicion, numeroedicion, fecha, url)
+            super(autores, titulo, serie, edicion, numeroedicion, fecha, [])
+            @url = url
+        end
     end
 end
--- spec/ebook_spec.rb
+++ spec/ebook_spec.rb
@@ -5,5 +5,13 @@ describe Bibliografia do
 	describe Bibliografia::Ebook do
 		before :all do
 		end
+		
+		it "Debe poderse pasarle todos los argumentos necesarios" do
+		    expect(Bibliografia::Ebook.new(["Dave Thomas"],
+			"Programming Ruby ",
+			"The Facets of Ruby",
+			"Pragmatic Bookshelf",4,Date.new(2013,7,7),
+			"google.es")).to_not be nil
+		end
 	end
 end
