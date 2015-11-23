9d3de7bec2b62d568f20beed6b4cc65e3718b12c
Eleazar Díaz Delgado <eleazardzdo@gmail.com>
2015-11-23 17:49:36 +0000

    Añado las expectativas para que se acomparable bibliografia

---
+++ spec/bibliografia_comparable_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe "Comparable overload methods" do
+  before :each do
+    @git = Bibliografia::Bibliografia.new(["Scott Chacon"],
+                                          "Pro Git 2009th Edition",
+                                          "Pro",
+                                          "Apress",
+                                          2009,
+                                          Date.new(2009,8,27),
+                                          ["ISBN-13: 9781430218333", "ISBN-12: 1430218339"])
+
+    @gitCompar = Bibliografia::Bibliografia.new(["Scott Chacon"],
+                                                "Pro Git 2009th Edition",
+                                                "Pro",
+                                                "Apress",
+                                                2009,
+                                                Date.new(2009,8,27),
+                                                ["ISBN-13: 9781430218333", "ISBN-12: 1430218339"])
+
+    @git2 =Bibliografia::Bibliografia.new(["Richard E. Silverman"],
+                                          "Git Pocket Guide",
+                                          nil,
+                                          "O'Really Media",
+                                          1,
+                                          Date.new(2013, 8, 2),
+                                          ["ISBN-10: 1449325866","ISBN-13: 978-1449325862"])
+  end
+
+  it "Debe de ser posible comparar mayor que, menor que " do
+    expect(@git2 > @git).to be false
+    expect(@gitCompar < @git).to be false
+    expect(@git2 < @git).to be true
+  end
+
+  it "Debe de ser posile comparar == <>" do
+    expect(@git == @gitCompar).to be true
+    expect(@git != @git2).to be true
+  end
+
+end
